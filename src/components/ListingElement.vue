f7357e5f13d06b0d8473553f734db950f9236812
gamelikerhd@gmail.com <Pi77amann>
2022-06-26 22:17:14 +0200

    erweiterung lexicon

--- src/components/ListingElement.vue
+++ src/components/ListingElement.vue
@@ -8,15 +8,11 @@
       </div>
 
       <div>
-
-      </div>
-
-      <div>
         <h4> Days until Plant need to be watered again: <span class="badge bg-success rounded-pill justify-content-flex end">6 </span></h4>
         <div class="d-grid gap-2 col-14">
-          <button type="button" class="btn-sm btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-droplet" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M7.21.8C7.69.295 8 0 8 0c.109.363.234.708.371 1.038.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8zm.413 1.021A31.25 31.25 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10a5 5 0 0 0 10 0c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"/>
-            <path fill-rule="evenodd" d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448z"/>
+          <button type="button" class="btn-sm btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+            <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
           </svg> watered </button>
         </div>
 
--- src/views/PlantLexicon.vue
+++ src/views/PlantLexicon.vue
@@ -1,18 +1,28 @@
 <template>
- <h1>Willkommen zum großen Pflanzenlexikon</h1>
+  <div class="list-group-item list-group-item-success">
+  <h1>Willkommen zum großen Pflanzenlexikon</h1>
   <div class="container-fluid">
     <div class="row row-cols-1 row-cols-md-2 g-4">
       <div class="col" v-for="plant in plantLexicon" :key="plant.id">
           <div class="card h-100">
             <img :src="getAvatar(plant)" class="card-img-top" :alt="plant.commonName + ' ' + plant.botanicalName">
-            <div class="card-body">
+            <div class="card-body" >
               <h5 class="card-title">{{plant.commonName}} oder auch {{plant.botanicalName}}</h5>
-              <p class="card-text">Das ist die {{plant.botanicalName}} oder zu Deutsch {{plant.commonName}}. Diese Pflanze wurde {{plant.isSaved ? 'gespeichert': 'noch nicht gespeichert'}}</p>
-              <button type="button" class="btn btn-success">Erfahre Mehr</button>
+              <p class="card-text">Das ist die {{plant.botanicalName}} oder zu Deutsch {{plant.commonName}}.
+                <br>Diese Pflanze wurde {{plant.isSaved ? 'gespeichert': 'noch nicht gespeichert'}}</p>
+             <div class ="card-text">
+               <button type="button" class="btn btn-success">Erfahre Mehr</button>
+                <p></p>
+                <button type="button" class="btn btn-primary btn-sm">add to Wateringchecklist <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-droplet" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M7.21.8C7.69.295 8 0 8 0c.109.363.234.708.371 1.038.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8zm.413 1.021A31.25 31.25 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10a5 5 0 0 0 10 0c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"/>
+                  <path fill-rule="evenodd" d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448z"/>
+                </svg></button>
+              </div>
             </div>
           </div>
         </div>
     </div>
+  </div>
   </div>
 </template>
 
