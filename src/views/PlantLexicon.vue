946809db0869b10d976b3d518bb6c25f7115edd3
gamelikerhd@gmail.com <Pi77amann>
2022-06-28 16:10:20 +0200

    plfanzen auf watering liste setzten geht

--- src/views/PlantLexicon.vue
+++ src/views/PlantLexicon.vue
@@ -14,10 +14,10 @@
              <div class =" mb-1 card-text">
                <button type="button" class="btn btn-success">Erfahre Mehr</button>
                 <p></p>
-                <button type="button" class="btn btn-primary disabled btn-sm">add to Wateringchecklist <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-droplet" viewBox="0 0 16 16">
+                <button type="submit" class="btn btn-primary btn-sm" @click="savePlant(plant)">add to Wateringchecklist <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-droplet" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M7.21.8C7.69.295 8 0 8 0c.109.363.234.708.371 1.038.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8zm.413 1.021A31.25 31.25 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10a5 5 0 0 0 10 0c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"/>
                   <path fill-rule="evenodd" d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448z"/>
-                </svg></button>
+                </svg> </button>
               </div>
             </div>
           </div>
@@ -29,7 +29,7 @@
 </template>
 
 <script>
-
+/*eslint-disable*/
 export default {
   name: 'PlantLexicon',
   data () {
@@ -46,6 +46,36 @@ export default {
       } else if (plant.botanicalName === 'Sanseviera') {
         return require('../assets/Sanseviera.jpg')
       }
+    },
+    test (plant) {
+      console.log(plant.commonName)
+    },
+    savePlant (plant) {
+
+      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/plant/' + plant.id
+
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      const raw = JSON.stringify({
+        "commonName": plant.commonName,
+        "botanicalName": plant.botanicalName,
+        "description": plant.description,
+        "wateringperiod": plant.wateringperiod,
+        "saved": true
+      });
+
+      const requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+
+      fetch(endpoint, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
     }
   },
   mounted () {
