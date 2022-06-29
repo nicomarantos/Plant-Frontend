fc1c8cd5263cb04e7043e0ffe88eb80ba9583169
gamelikerhd@gmail.com <Pi77amann>
2022-06-29 18:35:30 +0200

    valiidierung komplett

--- src/components/offcanvas.vue
+++ src/components/offcanvas.vue
@@ -8,24 +8,38 @@
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
-      <form class="text-start">
+      <form class="text-start needs-validation" novalidate>
       <div class="mb-3">
-        <label for="formGroupExampleInput" class="form-label">Botancial Name</label>
-        <input type="text" class="form-control" id="formGroupExampleInput" v-model="botanicalName">
+        <label for="formGroupExampleInput" class="form-label needs-validation">Botancial Name</label>
+        <input type="text" class="form-control" id="formGroupExampleInput" v-model="botanicalName" required>
+        <div class="invalid-feedback">
+          Please provide a Botancialname.
+        </div>
+      </div>
+
+      <div class="mb-3">
+        <label for="formGroupExampleInput2" class="form-label needs-validation">Common Name</label>
+        <input type="text" class="form-control" id="formGroupExampleInput2" v-model="commonName" required>
+        <div class="invalid-feedback">
+          Please provide a Commonname.
+        </div>
       </div>
       <div class="mb-3">
-        <label for="formGroupExampleInput2" class="form-label">Common Name</label>
-        <input type="text" class="form-control" id="formGroupExampleInput2" v-model="commonName">
+        <label for="formGroupExampleInput" class="form-label needs-validation">Description</label>
+        <input type="text" class="form-control" id="formGroupExampleInput" v-model="description" required>
+        <div class="invalid-feedback">
+          Please provide a Description.
+        </div>
       </div>
       <div class="mb-3">
-        <label for="formGroupExampleInput" class="form-label">Description</label>
-        <input type="text" class="form-control" id="formGroupExampleInput" v-model="description">
+        <label for="formGroupExampleInput2" class="form-label needs-validation" >Wateringperiod</label>
+        <input type="number" class="form-control" id="formGroupExampleInput2" v-model="wateringperiod" required>
+        <div class="invalid-feedback">
+          It need to be a number.
+        </div>
       </div>
-      <div class="mb-3">
-        <label for="formGroupExampleInput2" class="form-label">Wateringperiod</label>
-        <input type="text" class="form-control" id="formGroupExampleInput2" v-model="wateringperiod">
-      </div>
-      <br>
+
+        <br>
       <div class="mt-1 d-flex w-100 justify-content-between">
         <button class="btn btn-success" type="submit" @click="saveCustomPlant">save plant</button>
         <button class="btn btn-danger" type="reset" @click="saveCustomPlant">reset</button>
@@ -55,34 +69,51 @@ export default {
       this.wateringperiod= null
     },
     saveCustomPlant(){
+      const valid = this.validate()
+      if(valid===true) {
+        const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/plant/'
 
-      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/plant/'
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
 
-      const myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
+        const raw = JSON.stringify({
+          "commonName": this.commonName,
+          "botanicalName": this.botanicalName,
+          "description": this.description,
+          "wateringperiod": this.wateringperiod,
+          "wateringperiodCurrent": this.wateringperiod,
+          "saved": false
+        });
 
-      const raw = JSON.stringify({
-        "commonName": this.commonName,
-        "botanicalName": this.botanicalName,
-        "description": this.description,
-        "wateringperiod": this.wateringperiod,
-        "wateringperiodCurrent": this.wateringperiod,
-        "saved": false
-      });
+        const requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: raw,
+          redirect: 'follow'
+        };
 
-      const requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-      };
-
-      fetch(endpoint, requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-
-    }
+        fetch(endpoint, requestOptions)
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.log('error', error));
+      }
+    },
+    validate(){
+      let valid = true
+      var forms = document.querySelectorAll('.needs-validation')
+      // Loop over them and prevent submission
+      Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+          if (!form.checkValidity()) {
+              valid = false
+              event.preventDefault()
+              event.stopPropagation()
+            }
+          form.classList.add('was-validated')
+        }, false)
+      })
+      return valid
+    },
 
   }
 }
