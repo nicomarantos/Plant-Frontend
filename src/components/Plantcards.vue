65f843b9952e5651045931fef0aca94945f9f9c1
Max Hergete <Pi77amann>
2022-07-04 18:13:16 +0200

    englisch

--- src/components/Plantcards.vue
+++ src/components/Plantcards.vue
@@ -8,11 +8,11 @@
             <h3 class="card-title">{{plant.commonName}}</h3>
             <img :src="getAvatar(plant)" class="card-img-top" :alt="plant.botanicalName">
 
-            <p class="card-text">The {{plant.botanicalName}} or more commonly known as {{plant.commonName}}.</p>
+            <p class="card-text">The {{plant.botanicalName}} or commonnly known as {{plant.commonName}}.</p>
             <div class =" mb-1 card-text">
               <H6>{{plant.description}}</H6>
               <p></p>
-              <br>(This plant is {{plant.saved ? 'already saved': 'not saved yet'}})
+              <br>(The plant is {{plant.saved ? 'already saved': 'not saved yet'}})
               <br>
               <button :disabled="plant.saved" type="submit" class="btn btn-primary btn-sm" @click="savePlant(plant)">add to Wateringchecklist <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-droplet" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M7.21.8C7.69.295 8 0 8 0c.109.363.234.708.371 1.038.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8zm.413 1.021A31.25 31.25 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10a5 5 0 0 0 10 0c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"/>
@@ -36,6 +36,7 @@ export default {
       required: true
 
     }
+
   },
   methods: {
     getAvatar (plant) {
